966a451222e5e514f758e233fe63afd441feea07
Alan <alancurtis5000+accounts@gmail.com>
2023-05-03 22:37:13 -0600

    Updates

---
+++ src/events/order-cancelled-event.ts
@@ -0,0 +1,12 @@
+import { Subjects } from "./subjects";
+
+// each event will have its own event that handles type validation
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
+}
---
+++ src/events/order-created-event.ts
@@ -0,0 +1,17 @@
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
+
+// each event will have its own event that handles type validation
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
