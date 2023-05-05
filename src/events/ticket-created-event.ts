6d42be2ef386031bbdefe090968207bfd4af755c
Alan <alancurtis5000+accounts@gmail.com>
2023-05-05 13:15:35 -0600

    Updates

--- src/events/ticket-created-event.ts
+++ src/events/ticket-created-event.ts
@@ -5,6 +5,7 @@ export interface TicketCreatedEvent {
   subject: Subjects.TicketCreated;
   data: {
     id: string;
+    verion: number;
     title: string;
     price: number;
     userId: string;
